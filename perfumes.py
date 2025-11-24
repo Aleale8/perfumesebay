60a71334110b7f0c846ad4c440b68bc019c9c5fb
jrios607 <sage09664@gmail.com>
2025-11-24 15:51:21 -0300

    Update perfumes.py

--- perfumes.py
+++ perfumes.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt 
 import re
+import matplotlib.pyplot as plt  # <--- ¡ESTA ES LA LÍNEA QUE FALTA!
 
 # --- CONFIGURACIÓN DE PÁGINA ---
 st.set_page_config(page_title="Perfumes eBay", layout="wide")
